4b3626679865b9d607313d2bfd7570df1586656b
alumezi <arbnorlumezi@gmail.com>
2020-08-12 13:46:22 +0200

    4.1 & 4.2

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const Blog = require('./models/blog')
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const config = require('./utils/config')
+const BlogRouter = require('./controllers/blog')
+
+mongoose.connect(config.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', BlogRouter);
+
+module.exports = app;
