ee595e27a22bbed28b7c3ac894bfc9bde24126ea
alumezi <arbnorlumezi@gmail.com>
2020-08-12 13:27:43 +0200

    4.1

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+
+const mongoUrl = process.env.DB_URL;
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT || 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
