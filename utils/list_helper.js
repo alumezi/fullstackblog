b2f78babd021fd96b82dbc97aae9dc5d4087fc75
alumezi <arbnorlumezi@gmail.com>
2020-08-13 09:13:34 +0200

    4.5* : Favorite blog.

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,16 @@ const dummy = (blogs) => 1;
 
 const totalLikes = blogs => blogs.reduce((acc, curr) => acc + curr.likes, 0);
 
+const favoriteBlog = blogs => blogs.reduce((acc, curr) => {
+    if (acc.likes > curr.likes) {
+        return acc
+    } else {
+        return curr
+    }
+});
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
