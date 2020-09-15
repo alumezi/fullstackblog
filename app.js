9f8378de4514cf67f7220966bef7f0a486cc1d71
alumezi <arbnorlumezi@gmail.com>
2020-09-15 22:23:29 +0200

    4.15

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
 const config = require('./utils/config')
-const BlogRouter = require('./controllers/blog')
+const BlogRouter = require('./controllers/blogs')
+const UserRouter = require('./controllers/users')
 const { requestLogger, unknownEndpoint, errorHandler } = require('./utils/middleware')
 
 mongoose.connect(config.DB_URL, { useFindAndModify: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true })
@@ -12,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 app.use('/api/blogs', BlogRouter);
+app.use('/api/users', UserRouter);
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
